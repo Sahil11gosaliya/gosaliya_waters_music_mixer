3fd75443aefa3b3f6e71d0531074f37e172ba530
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-04-08 21:31:17 -0400

    Finished the requirements
    Completed all the tasks

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # gosaliya_waters_music_mixer
 A project file for music player
+
+# Tunebox - Music Mixer
+Tunebox is a web-based music mixer that allows you to play, pause, skip tracks, and adjust the volume. It also has a playlist of popular movie soundtracks that you can choose from. Additionally, you can drag and drop audio files into the drop area to play them in the player.
+
+# Features
+Play, pause, skip tracks, and adjust the volume
+Playlist of popular movie soundtracks
+Drag and drop audio files into the player
+Easy to use interface
+
+# Getting Started
+To get started with Tunebox, simply download the project files and open the index.html file in your web browser.
+
+# How to Use
+To play a track from the playlist, click on the corresponding image in the playlist.
+To adjust the volume, move the slider control.
+To play, pause, skip to the next or previous track, click on the corresponding button.
+To drag and drop an audio file into the player, drag the file onto the drop area.
+
+# Technologies Used
+HTML5
+CSS3
+JavaScript
+
+# Contributors
+This project was created by Sahilkumar Gosaliya: Designer & Jack Waters: Developer.
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
--- java/main.js
+++ java/main.js
@@ -1,3 +1,4 @@
+// Getting all necessary DOM elements
 const audioPlayer = document.getElementById("audio-player");
 const playBtn = document.getElementById("play-btn");
 const pauseBtn = document.getElementById("pause-btn");
@@ -5,79 +6,120 @@ const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 const volumeControl = document.getElementById("volume-control");
 const playlistItems = document.querySelectorAll("#playlist-items li");
+const dropArea = document.getElementById("drop-area");
 
+// Initializes currentTrack to the first item in the playlist
 let currentTrack = 0;
 
+// Loads the currently selected track into the audio player
 function loadTrack() {
-  audioPlayer.src = playlistItems[currentTrack].getAttribute("data-src");
-  audioPlayer.load();
+audioPlayer.src = playlistItems[currentTrack].getAttribute("data-src");
+audioPlayer.load();
 }
 
+// Playing the currently selected track
 function playTrack() {
-  audioPlayer.play();
+audioPlayer.play();
 }
 
+// Pause the currently playing track
 function pauseTrack() {
-  audioPlayer.pause();
+audioPlayer.pause();
 }
 
+// Going to the next track in the playlist
 function nextTrack() {
-  currentTrack++;
-  if (currentTrack > playlistItems.length - 1) {
-    currentTrack = 0;
-  }
-  loadTrack();
-  playTrack();
-  setActiveTrack();
+currentTrack++;
+if (currentTrack > playlistItems.length - 1) {
+currentTrack = 0;
+}
+loadTrack();
+playTrack();
+setActiveTrack();
 }
 
+// Going to the previous track in the playlist
 function prevTrack() {
-  currentTrack--;
-  if (currentTrack < 0) {
-    currentTrack = playlistItems.length - 1;
-  }
-  loadTrack();
-  playTrack();
-  setActiveTrack();
+currentTrack--;
+if (currentTrack < 0) {
+currentTrack = playlistItems.length - 1;
+}
+loadTrack();
+playTrack();
+setActiveTrack();
 }
 
+// Setting the volume of the audio player
 function setVolume() {
-  audioPlayer.volume = volumeControl.value;
+audioPlayer.volume = volumeControl.value;
 }
 
+// Setting the active track in the playlist
 function setActiveTrack() {
-  playlistItems.forEach((item) => {
-    item.classList.remove("active");
-  });
-  playlistItems[currentTrack].classList.add("active");
+playlistItems.forEach((item) => {
+item.classList.remove("active");
+});
+playlistItems[currentTrack].classList.add("active");
 }
 
+// Playing the next track when the current track ends
 function playNextTrack() {
-  if (currentTrack < playlistItems.length - 1) {
-    nextTrack();
-  } else {
-    currentTrack = 0;
-    loadTrack();
-    playTrack();
-    setActiveTrack();
-  }
+if (currentTrack < playlistItems.length - 1) {
+nextTrack();
+} else {
+currentTrack = 0;
+loadTrack();
+playTrack();
+setActiveTrack();
+}
 }
 
+// Adding an event listener to the audio player to play the next track when the current track ends
 audioPlayer.addEventListener("ended", playNextTrack);
 
+// Loading the first track in the playlist
 loadTrack();
 
+// Adding event listeners for all necessary DOM elements
 playBtn.addEventListener("click", playTrack);
 pauseBtn.addEventListener("click", pauseTrack);
 nextBtn.addEventListener("click", nextTrack);
 prevBtn.addEventListener("click", prevTrack);
 volumeControl.addEventListener("input", setVolume);
 
+// Adding click event listeners to all playlist items
 playlistItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    currentTrack = index;
-    loadTrack();
-    playTrack();
-    setActiveTrack();
-  });
+item.addEventListener("click", () => {
+currentTrack = index;
+loadTrack();
+playTrack();
+setActiveTrack();
 });
+
+// Making playlist items draggable
+item.setAttribute("draggable", "true");
+item.addEventListener("dragstart", (event) => {
+event.dataTransfer.setData("text/plain", index);
+});
+});
+
+// Preventing the default dragover behavior for the drop area
+dropArea.addEventListener("dragover", (event) => {
+event.preventDefault();
+});
+
+// Updated the 'drop' event listener 
+dropArea.addEventListener("drop", (event) => {
+event.preventDefault();
+const newIndex = event.dataTransfer.getData("text/plain");
+currentTrack = parseInt(newIndex, 10);
+loadTrack();
+playTrack();
+setActiveTrack();
+
+toggleDropAreaActive();
+setTimeout(() => {
+toggleDropAreaActive();
+}, 1000);
+});
+
